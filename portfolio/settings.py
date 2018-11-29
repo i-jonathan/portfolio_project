d62a42b440b272a05585d01085e3c08b566dea68
Jay <FarinloyeJonathan@gmail.com>
2018-11-29 10:34:45 +0100

    Switch to Postgres

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Django1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
